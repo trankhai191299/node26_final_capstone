a8a9f0e642c2e3bb7e345d4384c96ddeda390eb6
khaitran <akashikazuo99@gmail.com>
2023-02-05 19:08:30 +0700

    nguoi dung xong

--- src/routers/v1/ngdung.router.js
+++ src/routers/v1/ngdung.router.js
@@ -5,14 +5,14 @@ const upload = require('../../middlewares/upload');
 const reqRole = require("../../middlewares/requireRole");
 
 //API nguoi dung
-ngDungRouter.get('/',ngDungController.getAllNgDung())
-ngDungRouter.post('/',reqRole('ADMIN'),ngDungController.createNgDung())
-ngDungRouter.post('/upload-avatar',upload.single('file'),ngDungController.uploadImg())
-ngDungRouter.delete('/',reqRole('ADMIN'),ngDungController.deleteNgDung())
-ngDungRouter.get('/phan-trang-tim-kiem',ngDungController.paginate())
-ngDungRouter.get('/:id',ngDungController.getNgDungbyId())
-ngDungRouter.put('/',ngDungController.updateNgDung())
-ngDungRouter.get('/search/:keyword',ngDungController.searchNgDung())
+ngDungRouter.get('/',ngDungController.getAllNgDung()) // get all ng dung
+ngDungRouter.post('/',reqRole('ADMIN'),ngDungController.createNgDung()) // create ng dung
+ngDungRouter.post('/upload-avatar',upload.single('file'),ngDungController.uploadImg()) // upload avatar
+ngDungRouter.delete('/',reqRole('ADMIN'),ngDungController.deleteNgDung()) // delete ng dung
+ngDungRouter.get('/phan-trang-tim-kiem',ngDungController.paginate()) // phan trang
+ngDungRouter.get('/:id',ngDungController.getNgDungbyId()) // get ng dung by id
+ngDungRouter.put('/',ngDungController.updateNgDung()) // update ng dung
+ngDungRouter.get('/search/:keyword',ngDungController.searchNgDung()) // search ng dung theo ten
 
 
 module.exports = ngDungRouter
