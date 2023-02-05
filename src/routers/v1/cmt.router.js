25ca547d8ec686569476f01eea74a584481c1f0a
khaitran <akashikazuo99@gmail.com>
2023-02-05 19:12:04 +0700

    them cmt o router

--- src/routers/v1/cmt.router.js
+++ src/routers/v1/cmt.router.js
@@ -3,10 +3,10 @@ const cmtController = require('../../controllers/BinhLuan.controller')
 const cmtRouter = express.Router()
 
 // API binh luan
-cmtRouter.get('/',cmtController.getAllBinhLuan())
-cmtRouter.post('/',cmtController.createBinhLuan())
-cmtRouter.put('/',cmtController.updateBinhLuan())
-cmtRouter.delete('/:id',cmtController.deleteBinhLuan())
-cmtRouter.get('/lay-binh-luan-theo-cong-viec/:id',cmtController.getBinhLuanbyJobId())
+cmtRouter.get('/',cmtController.getAllBinhLuan()) // get all binh luan
+cmtRouter.post('/',cmtController.createBinhLuan()) // create binh luan
+cmtRouter.put('/',cmtController.updateBinhLuan()) // update binh luan
+cmtRouter.delete('/:id',cmtController.deleteBinhLuan()) // delete binh luan
+cmtRouter.get('/lay-binh-luan-theo-cong-viec/:id',cmtController.getBinhLuanbyJobId()) // get binh luan by job id
 
 module.exports = cmtRouter
--- src/routers/v1/congviec.router.js
+++ src/routers/v1/congviec.router.js
@@ -3,14 +3,14 @@ const conviecController = require('../../controllers/CongViec.controller')
 const congviecRouter = express.Router()
 
 // API cong viec
-congviecRouter.get('/',conviecController.getAllCv())
-congviecRouter.post('/',conviecController.createCv())
-congviecRouter.get('/phan-trang-tim-kiem',conviecController.paginate())
-congviecRouter.get('/lay-menu-loai-cong-viec',conviecController.getMenuLoaiCv())
-congviecRouter.get('/lay-chi-tiet-loai-cong-viec/:id',conviecController.getDetailsbyType())
-congviecRouter.get('/lay-cong-viec-theo-chi-tiet-loai/:id',conviecController.getCvbyDetail())
-congviecRouter.get('/lay-danh-sach-cong-viec-theo-ten/:keyword',conviecController.searchCv())
-congviecRouter.get('/:id',conviecController.getCvById())
-congviecRouter.put('/:id',conviecController.updateCv())
-congviecRouter.delete('/:id',conviecController.deleteCv())
+congviecRouter.get('/',conviecController.getAllCv()) // get all cong viec
+congviecRouter.post('/',conviecController.createCv()) // create cong viec
+congviecRouter.get('/phan-trang-tim-kiem',conviecController.paginate()) // phan trang
+congviecRouter.get('/lay-menu-loai-cong-viec',conviecController.getMenuLoaiCv()) // get menu loai cong viec
+congviecRouter.get('/lay-chi-tiet-loai-cong-viec/:id',conviecController.getDetailsbyType()) // get chi tiet theo ma loai cv
+congviecRouter.get('/lay-cong-viec-theo-chi-tiet-loai/:id',conviecController.getCvbyDetail()) // get cv theo ma chi tiet
+congviecRouter.get('/lay-danh-sach-cong-viec-theo-ten/:keyword',conviecController.searchCv()) // get cong viec theo ten
+congviecRouter.get('/:id',conviecController.getCvById()) // get cv by id
+congviecRouter.put('/:id',conviecController.updateCv()) // update cv
+congviecRouter.delete('/:id',conviecController.deleteCv()) // delete cv
 module.exports = congviecRouter
